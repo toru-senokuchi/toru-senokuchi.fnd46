28c3a4fc141887a5cbb6eca28d76f2b1e1a09799
toru-senokuchi <toru.senokuchi.1@toyota.global>
2026-04-21 11:47:44 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+// 1行目に記載している "use strict" は削除しないでください
+
+/*仮説を立てて、まずは試してみる！できるだけ手をとめない
+  1人でやっている時も、誰かに対して論理的に説明できるようにする
+  わからない時を楽しむ*/
+
+
+// let actual;
+// let expected;
+
+
+// function test(actual, expected) {
+//   if (JSON.stringify(actual) === JSON.stringify(expected)) {
+//     console.log("OK! Test PASSED.");
+//   } else {
+//     console.error("Test FAILED. Try again!");
+//     console.log("    actual: ", actual);
+//     console.log("  expected: ", expected);
+//     console.trace();
+//   }
+// }
